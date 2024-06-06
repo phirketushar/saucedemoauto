6575adcafde9979d226f1b4137737792c661257f
phirketushar <phirke.tushar@gmail.com>
2024-06-06 17:18:41 +0530

    Update for data_file.py
    selected count increased from 2 to 3

--- sauce_lab_automation_assignment/configs/data_file.py
+++ sauce_lab_automation_assignment/configs/data_file.py
@@ -4,7 +4,7 @@ product_purchase = [
   {
     "username": "standard_user",
     "password": "secret_sauce",
-    "product_to_select": 2,
+    "product_to_select": 3,
     "checkout_addr_first_name": "test1",
     "checkout_addr_last_name": "test2",
     "checkout_addr_zip_code": "444"
